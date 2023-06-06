925b4f4136c12a353f02e0b973ccc70b7e399721
bthecs <lautarogimenez897@gmail.com>
2023-06-06 00:57:45 -0300

    add final report

--- main/services/custom.py
+++ main/services/custom.py
@@ -9,22 +9,18 @@ Custom cce, plate_acc and acc for plate recognition using CNN
 
 
 def cat_acc(y_true, y_pred):
+    # Reorganiza los tensores de verdadero y predicho en una forma adecuada.
     y_true = K.reshape(y_true, shape=(-1, 7, 37))
     y_pred = K.reshape(y_pred, shape=(-1, 7, 37))
+    # Calcula la precisión categórica utilizando las métricas incorporadas de Keras.
     return K.mean(tf.keras.metrics.categorical_accuracy(y_true, y_pred))
 
 
 def plate_acc(y_true, y_pred):
-    '''
-    How many plates were correctly classified
-    If Ground Truth is ABC 123
-    Then prediction ABC 123 would score 1
-    else ABD 123 would score 0
-    Avg these results (1 + 0) / 2 -> Gives .5 accuracy
-    (Half of the plates were completely corrected classified)
-    '''
+    # Reorganiza los tensores de verdadero y predicho en una forma adecuada.
     y_true = K.reshape(y_true, shape=(-1, 7, 37))
     y_pred = K.reshape(y_pred, shape=(-1, 7, 37))
+    # Compara las predicciones con los valores verdaderos y calcula la precisión de la placa.
     et = K.equal(K.argmax(y_true), K.argmax(y_pred))
     return K.mean(
         K.cast(K.all(et, axis=-1, keepdims=False), dtype='float32')
@@ -32,9 +28,10 @@ def plate_acc(y_true, y_pred):
 
 
 def top_3_k(y_true, y_pred):
-    # Reshape into 2-d
+    # Reorganiza los tensores de verdadero y predicho en una forma adecuada.
     y_true = K.reshape(y_true, (-1, 37))
     y_pred = K.reshape(y_pred, (-1, 37))
+    # Calcula la precisión top-3 utilizando la métrica incorporada de Keras.
     return K.mean(
         tf.keras.metrics.top_k_categorical_accuracy(y_true, y_pred, k=3)
     )
@@ -42,9 +39,10 @@ def top_3_k(y_true, y_pred):
 
 # Custom loss
 def cce(y_true, y_pred):
+    # Reorganiza los tensores de verdadero y predicho en una forma adecuada.
     y_true = K.reshape(y_true, shape=(-1, 37))
     y_pred = K.reshape(y_pred, shape=(-1, 37))
-
+    # Calcula la pérdida de entropía cruzada categórica utilizando la función de pérdida incorporada de Keras.
     return K.mean(
         tf.keras.losses.categorical_crossentropy(
             y_true, y_pred, from_logits=False, label_smoothing=0.2
--- main/services/ocr.py
+++ main/services/ocr.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras.activations import softmax
-from custom import cce, cat_acc, plate_acc, top_3_k
+from .custom import cce, cat_acc, plate_acc, top_3_k
 
 class DetectOCR:
 
@@ -18,40 +18,48 @@ class DetectOCR:
             'top_3_k': top_3_k,
             'softmax': softmax
         }
-        self.model = tf.keras.models.load_model('model.h5', custom_objects=self.custom_objects)
+        self.model = tf.keras.models.load_model('main\services\model.h5', custom_objects=self.custom_objects)
     
     
     
     @tf.function
     def predict_array(self, img):
-        
+        # Realiza una inferencia de placas utilizando el modelo cargado y la imagen de entrada.
         predictions = self.model(img, training=False)
         return predictions
     
     def plate_probs(self, prediction):
+        # Reorganiza la forma de la predicción en (7, 37) para reflejar las dimensiones de la placa.
         prediction = prediction.reshape((7,37))
+        # Calcula las probabilidades máximas para cada posición de la placa.
         probs = np.max(prediction, axis=1)
+        # Obtiene el índice de la letra con la probabilidad más alta para cada posición de la placa.
         prediction = np.argmax(prediction, axis=-1)
+        # Mapea los índices de las letras a las letras correspondientes utilizando el atributo "alphabet".
         plate = list(map(lambda x: self.alphabet[x], prediction))
+        # Devuelve las probabilidades y las letras de la placa.
         return probs, plate
     
     def process_roi(self, image):
         
         #cambiar imagen a escala de grises
         img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # redimensionar la imagen
         img = cv2.resize(img, dsize=(140,70), interpolation=cv2.INTER_LINEAR)
+        # agregar una dimensión al array para que cumpla con el modelo
         img = img[np.newaxis, ..., np.newaxis] / 255.
+        # Convierte el tensor de datos a un tensor constante de TensorFlow
         plate = tf.constant(img, dtype=tf.float32)
+        # Realiza una inferencia de placas utilizando el modelo cargado y la imagen de entrada.
         prediction = self.predict_array(plate).numpy()
+        # Obtiene las probabilidades de las placas en la imagen
         plate_probs, plate = self.plate_probs(prediction)
-        print(plate)
-        plate_final = ''.join(plate).strip('_')
-        print(f"Plate: {plate_final}")
-        print(f"Probs: {plate_probs}")
-        
+        # print(plate)
+        plate_final = ''.join(plate).strip('_')        
         return plate_final
     
 
     def main(self, image):        
-        self.process_roi(image)
+        plate_final = self.process_roi(image)
+        return plate_final
 
